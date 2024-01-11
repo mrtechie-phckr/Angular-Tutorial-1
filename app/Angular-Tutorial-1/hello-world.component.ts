d1d7337fb55352e2d8e97e682bfaff682c8b8a93
mrtechie-phckr <155867262+mrtechie-phckr@users.noreply.github.com>
2024-01-11 14:49:48 +0530

    Add files via upload

---
+++ app/Angular-Tutorial-1/hello-world.component.ts
@@ -0,0 +1,54 @@
+import { Component } from "@angular/core";
+
+@Component ({
+    selector:'app-hello-world',
+    templateUrl:'./hello-world.component.html',
+    styleUrls:['./hello-world.component.css']
+})
+
+export class HelloWorldComponent{
+    title='hello world';
+    // DATA BINDING:
+     first_name='MANOJ KIYAN';
+     last_name='M'
+    gettitle(){
+         return this.last_name
+     }
+    getMax(first:number,second:number){
+       return Math.max(first,second)
+    }
+     clickcount=0;
+     clickme(){
+         this.clickcount++;
+
+     }
+    value=''
+    value1=''
+     handleinput(event:any){
+         this.value=(event.target as HTMLInputElement).value;
+     }
+     cinema='LIST OF MOVIES'
+
+    movies=[
+        {title:'LEO',dir:'LOKESH KANAGARAJ',releaseyear:'2023'},
+        {title:'JAILER',dir:'NELSON',releaseyear:'2023'},
+        {title:'DOCTOR',dir:'NELSON',releaseyear:'2022'},
+        {title:'12th-FAIL',dir:'vidhu vinod chopra',releaseyear:'2024'}
+    ]
+     num = 0;
+     showme=false;
+     enablecolor=false;
+     enablebig=false;
+     enableitalics=false;
+     status='error';
+     color='';
+    // PIPES:
+    chdate=new Date();
+    messageU='pipes in uppercase';
+    messageL='PIPES IN LOWERCASE';
+    price=2316;
+    // SHARING DATA BETWEEN COMPONENTS :
+    
+}
+
+    
---
+++ app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HelloWorldComponent } from './Angular-Tutorial-1/hello-world.component';
+import { FormsModule } from '@angular/forms';
+import { FormComponent } from './form/form.component';
+import { ListComponent } from './list/list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HelloWorldComponent,
+    FormComponent,
+    ListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
